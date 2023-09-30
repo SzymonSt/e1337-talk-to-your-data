981f146d9d00204fdd5b204d22f7a3df49de8b0c
Michał Kiełtyka <defozo@gmail.com>
2023-09-30 15:50:01 +0200

    Added get_last_action_input function to return sql query that was used

--- api/test_custom_sql_agent.py
+++ api/test_custom_sql_agent.py
@@ -1,4 +1,7 @@
 import unittest
+import io
+import sys
+import re
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.sql_database import SQLDatabase
@@ -19,7 +22,44 @@ class TestCustomSQLAgent(unittest.TestCase):
         self.agent = create_sql_agent(llm=self.llm, toolkit=self.toolkit, verbose=True)
 
     def test_execute_query(self):
+        # Redirect stdout to a string buffer
+        buffer = io.StringIO()
+        sys.stdout = buffer
+
+        # Run the query
         self.agent.run("What is the Employee with the longest name?")
+
+        # Get the logs from the buffer
+        logs = buffer.getvalue()
+
+        # Reset stdout to its original value
+        sys.stdout = sys.__stdout__
+
+        # Print the logs
+        print("Logs:")
+        print(logs)
+
+        # Now you can do whatever you want with the logs
+        print("Last action input: ")
+        print(self.get_last_action_input(logs))
+
+    def get_last_action_input(self, logs):
+        # Split the logs into lines
+        lines = logs.split("\n")
+
+        # Reverse the lines so we start from the end
+        lines.reverse()
+
+        # Iterate over the reversed lines
+        for line in lines:
+            # If the line starts with "Action Input:", return the rest of the line
+            if line.startswith("Action Input: "):
+                end_index = line.find(";")
+                if end_index != -1:
+                    return line[len("Action Input: "):end_index+1]
+
+        # If no "Action Input:" line is found, return None
+        return None
 
 if __name__ == '__main__':
     unittest.main()
